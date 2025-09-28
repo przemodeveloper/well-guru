17f5cc4c95026af30f1b4cfdbf33e3950d780cbf
przemodeveloper <przemyslaw.gorecki93@gmail.com>
2025-09-28 12:07:26 +0200

    Update TeaserItems container

--- components/Teaser.tsx
+++ components/Teaser.tsx
@@ -7,51 +7,51 @@ import Link from "next/link";
 import { ROUTES } from "@/routes/routes";
 
 export const teaserItems = [
-	{
-		title: "Wyjazdy",
-		image: retreat.src,
-	},
-	{
-		title: "Eventy",
-		image: event.src,
-	},
-	{
-		title: "Warsztaty",
-		image: workshop.src,
-	},
+  {
+    title: "Wyjazdy",
+    image: retreat.src,
+  },
+  {
+    title: "Eventy",
+    image: event.src,
+  },
+  {
+    title: "Warsztaty",
+    image: workshop.src,
+  },
 ];
 
 const Teaser = () => {
-	return (
-		<>
-			<div className="flex justify-between flex-wrap mb-10 gap-y-10">
-				<div className="flex gap-5 flex-wrap">
-					<h2 className="text-3xl font-bold max-w-[20ch]">
-						Odpoczywaj i doświadczaj świadomie
-					</h2>
-					<p className="text-lg font-bold max-w-[32ch] text-gray-500">
-						Selekcjonujemy wyjazdy, wydarzenia i warsztaty offline wspierające
-						dobrostan ciała i umysłu
-					</p>
-				</div>
+  return (
+    <>
+      <div className="flex justify-between flex-wrap mb-10 gap-y-10">
+        <div className="flex gap-5 flex-wrap">
+          <h2 className="text-3xl font-bold max-w-[20ch]">
+            Odpoczywaj i doświadczaj świadomie
+          </h2>
+          <p className="text-lg font-bold max-w-[32ch] text-gray-500">
+            Selekcjonujemy wyjazdy, wydarzenia i warsztaty offline wspierające
+            dobrostan ciała i umysłu
+          </p>
+        </div>
 
-				<div>
-					<Link
-						href={ROUTES.EVENTS}
-						className="bg-black text-white px-12 py-3 rounded-xl cursor-pointer text-nowrap"
-					>
-						Odkrywaj wydarzenia
-					</Link>
-				</div>
-			</div>
+        <div>
+          <Link
+            href={ROUTES.EVENTS}
+            className="bg-black text-white px-12 py-3 rounded-xl cursor-pointer text-nowrap"
+          >
+            Odkrywaj wydarzenia
+          </Link>
+        </div>
+      </div>
 
-			<div className="flex flex-wrap justify-center gap-5">
-				{teaserItems.map((item) => (
-					<TeaserItem key={item.title} title={item.title} image={item.image} />
-				))}
-			</div>
-		</>
-	);
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-2">
+        {teaserItems.map((item) => (
+          <TeaserItem key={item.title} title={item.title} image={item.image} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Teaser;
--- components/TeaserItem.tsx
+++ components/TeaserItem.tsx
@@ -1,25 +1,25 @@
 import Image from "next/image";
 
 interface TeaserItemProps {
-	title: string;
-	image: string;
+  title: string;
+  image: string;
 }
 
 const TeaserItem = ({ title, image }: TeaserItemProps) => {
-	return (
-		<div>
-			<div className="w-[400px] h-[400px] border border-gray-300 rounded mb-2">
-				<Image
-					src={image}
-					alt={title}
-					width={400}
-					height={400}
-					className="object-cover rounded h-full w-full"
-				/>
-			</div>
-			<h3 className="text-lg font-bold">{title}</h3>
-		</div>
-	);
+  return (
+    <div className="w-full h-full">
+      <div className="border border-gray-300 rounded mb-2 w-full h-[400px]">
+        <Image
+          src={image}
+          alt={title}
+          width={400}
+          height={400}
+          className="object-cover rounded h-full w-full"
+        />
+      </div>
+      <h3 className="text-lg font-bold">{title}</h3>
+    </div>
+  );
 };
 
 export default TeaserItem;
